aaf23dcbaafe134c2b68aee2bd9cfdbe3e6f43f9
LucassRodr1guess <84428281+LucassRodr1guess@users.noreply.github.com>
2023-04-24 10:06:13 -0300

    Add files via upload

---
+++ src/pages/contato.jsx
@@ -0,0 +1,11 @@
+import Menu from "./menu"
+
+function Contato(){
+    return(
+        <div>
+        <h1>Contato</h1>
+        <Menu/>  
+        </div>
+    )
+}
+export default Contato
---
+++ src/pages/sobre.jsx
@@ -0,0 +1,11 @@
+import Menu from "./menu"
+
+function Sobre(){
+    return(
+        <div>
+            <h1>Sobre</h1>
+            <Menu/>
+        </div>
+    )
+}
+export default Sobre
