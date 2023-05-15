063a274f25f3b2640e0c887087ec8f04089621ec
LucassRodr1guess <84428281+LucassRodr1guess@users.noreply.github.com>
2023-05-15 11:57:20 -0300

    Add files via upload

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -4,26 +4,33 @@ import "../App.css"
 function Sobre(){
     return(
     <div className="fl1">
-        <div>
-           <h1>Produtos Floricultura - SUNSET</h1>
-            <div class ="produtosFlores">
+        
+      
+            
+    <div className="fl2">
+    <h1>Produtos Floricultura - SUNSET</h1>
+      <div>
             <div class="card">
             <img src={'https://media.discordapp.net/attachments/960884773725831228/1104898303310712842/31882gg.webp'} alt="Workflow" />
             <h4><b>ARRANJO DE ORQUÍDEA BRANCA E ASPLÊNIO</b></h4>
             <p>R$150,00</p>
+        </div>
             </div>
-             
-            <div>
-            <div>
+            
+            
+           
+     
+            
+      <div>
             <div class="card">
             <img src={'https://media.discordapp.net/attachments/960884773725831228/1104900187857621022/25801gg.webp'} alt="Workflow" />
             <h4><b>ORQUÍDEA DENPHALE LILÁS PRESENTE</b></h4>
             <p>R$75,00</p>
         </div>
             </div>
-        </div>
+        
 
-        <div>
+        
            
             <div>
             <div class="card">
@@ -32,39 +39,39 @@ function Sobre(){
             <p>R$60,00</p>
         </div>
             </div>
-        </div>
-        </div>
         
-        <div>
-           
-            <div>
-            <div class="card">
-            <img src={'https://media.discordapp.net/attachments/960884773725831228/1104901211116810250/31841gg.webp'} alt="Workflow" />
-            <h4><b>MEGA BUQUÊ DE ROSAS VERMELHAS</b></h4>
-            <p>R$500,00</p>
-        </div>
-            </div>
-        </div>
-
-        <div>
+        
+        
+        
            
             <div>
             <div class="card">
             <img src={'https://media.discordapp.net/attachments/960884773725831228/1104900683402063892/27566gg.webp'} alt="Workflow" />
+            <h4><b>MEGA BUQUÊ DE ROSAS VERMELHAS</b></h4>
+            <p>R$500,00</p>
+        </div>
+            </div>
+        
+
+        
+           
+            <div>
+            <div class="card">
+            <img src={'https://media.discordapp.net/attachments/960884773725831228/1104901211116810250/31841gg.webp'} alt="Workflow" />
             <h4><b>LIRIO AMARELO PLANTADO NO VASO BOX GRAFISMO</b></h4>
             <p>R$100,00</p>
         </div>
             </div>
-        </div>
+        
 
         
       
-    </div>
+    
         <div class = "espaco">
         <a href="/" class="btn btn-primary">Clique para voltar para nossa página Inicial</a>&nbsp;&nbsp;&nbsp;
         <a href="/login" class="btn btn-primary">Clique para realizar um login</a>&nbsp;&nbsp;&nbsp;
         </div>
-        
+        </div>
         </div>
 
         
